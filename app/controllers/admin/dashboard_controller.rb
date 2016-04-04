168f21a40ed3dc4edbc3129b78a2fe210951379d
pmcguane <paul.mcguane@me.com>
2016-04-04 22:39:09 +1000

    admin tax rates

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,14 +1,3 @@
 class Admin::DashboardController < ApplicationController
   layout 'superman'
-  # before_action :ensure_user
-  # before_action :super_access
-
-  def index
-    # @suppliers = Supplier.all
-    @orders = OrderItem
-                  .joins(:variants)
-                  .joins(:variant_suppliers)
-                  .joins(:suppliers)
-  end
-
 end
