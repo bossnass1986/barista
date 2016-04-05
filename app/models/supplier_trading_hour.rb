daff8476f2a81ddc8bbc4a46386f087b013105e0
pmcguane <paul.mcguane@me.com>
2016-04-05 19:53:50 +1000

    model fixes

--- app/models/supplier_trading_hour.rb
+++ app/models/supplier_trading_hour.rb
@@ -1,6 +1,6 @@
 class SupplierTradingHour < ActiveRecord::Base
   belongs_to :supplier
 
-  scope :open, -> { where ("self.weekday = ? AND self.open_time >= ? AND self.close_time < ?", Date.today.wday, DateTime.now, DateTime.now) }
+  # scope :open, -> { where ("self.weekday = ? AND self.open_time >= ? AND self.close_time < ?", Date.today.wday, DateTime.now, DateTime.now) }
 end
 
