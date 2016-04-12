4fbac9cb224e599db2008bceb7536959a1db953a
pmcguane <paul.mcguane@me.com>
2016-04-12 17:27:01 +1000

    supplier style changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -168,22 +168,9 @@ OrderStatus.create!([
 
 puts 'Deleting All Order Items'
 OrderItem.delete_all
-# puts 'Creating Sample Order Items'
-# OrderItem.create!([
-#   {id: 1, price: 4.95, total: 4.95, order_id: 1, variant_id: 1},
-#   {id: 2, price: 5.95, total: 5.95, order_id: 1, variant_id: 2},
-# ])
-
 
 puts 'Deleting All Orders'
 Order.delete_all
-# puts 'Creating Sample Orders'
-# Order.create!([
-#   {id: 1, calculated_at: '2013-01-18 17:07:29', completed_at: '2013-01-18 18:55:00', user_id: '1', order_status_id: 1},
-#   {id: 2, calculated_at: '2013-01-22 15:56:08', completed_at: '2013-01-22 16:02:00', user_id: '1', order_status_id: 2},
-#   {id: 3, calculated_at: '2013-01-31 17:37:58', completed_at: '2013-02-06 22:22:00', user_id: '2', order_status_id: 3},
-#   {id: 4, completed_at: '2013-02-06 22:22:00', user_id: '1', order_status_id: 4}
-# ])
 
 puts 'She may not look like much, but she\'s got it where it counts, kid!'
 
