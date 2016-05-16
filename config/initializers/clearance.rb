c540b738a67e379c5719b497b11e32a19531622f
pmcguane <paul.mcguane@me.com>
2016-05-17 00:21:01 +1000

    added clearance to replace devise started to simplify translations

---
+++ config/initializers/clearance.rb
@@ -0,0 +1,19 @@
+Clearance.configure do |config|
+
+  config.allow_sign_up = true
+  config.cookie_domain = '.club.town'
+  config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
+  config.cookie_name = "remember_token"
+  config.cookie_path = "/"
+  config.routes = true
+  config.httponly = false
+  config.mailer_sender = 'Cup Town <info@cup.town>'
+  config.password_strategy = Clearance::PasswordStrategies::BCrypt
+  config.redirect_url = "/"
+  config.secure_cookie = false
+  config.sign_in_guards = []
+  config.user_model = User
+
+
+  Clearance::SessionsController.layout 'landing'
+end
