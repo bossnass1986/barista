2b43cd99f91f82f764b3a6626337b129d735d98b
pmcguane <paul.mcguane@me.com>
2016-06-02 23:13:10 +1000

    phonegap prep

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.middleware.insert 0, 'Rack::WWWhisper'
+  # config.middleware.insert 0, 'Rack::WWWhisper'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
