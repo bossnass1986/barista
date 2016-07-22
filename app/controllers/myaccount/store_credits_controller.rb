7ff1d8f1be0d2f1b43993352a65922fd180f336a
pmcguane <paul.mcguane@me.com>
2016-07-22 20:56:54 +1000

    sign up page changes

--- app/controllers/myaccount/store_credits_controller.rb
+++ app/controllers/myaccount/store_credits_controller.rb
@@ -4,41 +4,41 @@ class Myaccount::StoreCreditsController < Myaccount::BaseController
     @store_credit = customer.store_credit
   end
 
-  # def update
-  #   result = Braintree::Transaction.sale(
-  #       :amount => params[:amount_to_add].to_f,
-  #       # :order_id => "order id",
-  #       :customer_id => customer.customer_cim_id,
-  #       :tax_amount => (params[:amount_to_add].to_f / 11).round(2),
-  #       :options => {
-  #           :submit_for_settlement => true
-  #       }
-  #
-  #   )
-  #   if result.success?
-  #     logger.info "Added to #{params[:amount_to_add].to_f} to #{customer.first_name} #{customer.last_name} (#{customer.customer_cim_id})"
-  #     customer.store_credit.add_credit(params[:amount_to_add].to_f)
-  #     redirect_to myaccount_store_credit_path
-  #     # , :notice => "Successfully updated store credit."
-  #   else
-  #     result.errors.each do |error|
-  #       puts error.message
-  #       # @store_credit.errors.add(:base, error.message)
-  #       render :show, :notice => error.message
-  #     end
-  #   end
-  # end
   def update
-    @store_credit.process_braintree_payment(params[:amount_to_add])
-    if @store_credit.errors.empty?
+    result = Braintree::Transaction.sale(
+        :amount => params[:amount_to_add].to_f,
+        # :order_id => "order id",
+        :customer_id => customer.customer_cim_id,
+        :tax_amount => (params[:amount_to_add].to_f / 11).round(2),
+        :options => {
+            :submit_for_settlement => true
+        }
+    )
+    if result.success?
       logger.info "Added to #{params[:amount_to_add].to_f} to #{customer.first_name} #{customer.last_name} (#{customer.customer_cim_id})"
       customer.store_credit.add_credit(params[:amount_to_add].to_f)
       redirect_to myaccount_store_credit_path
       # , :notice => "Successfully updated store credit."
     else
-      render :show
+      result.errors.each do |error|
+        puts error.message
+        # @store_credit.errors.add(:base, error.message)
+        render :show, :notice => error.message
+      end
     end
   end
+
+  # def update
+  #   @store_credit.process_braintree_payment(customer.customer_cim_id,params[:amount_to_add])
+  #   if @store_credit.errors.empty?
+  #     logger.info "Added to #{params[:amount_to_add].to_f} to #{customer.first_name} #{customer.last_name} (#{customer.customer_cim_id})"
+  #     customer.store_credit.add_credit(params[:amount_to_add].to_f)
+  #     redirect_to myaccount_store_credit_path
+  #     # , :notice => "Successfully updated store credit."
+  #   else
+  #     render :show
+  #   end
+  # end
 
   private
 
